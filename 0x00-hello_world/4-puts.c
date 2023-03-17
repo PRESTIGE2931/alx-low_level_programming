7af4d816357df5ee2be7a8923ca05458a776a470
PRESTIGE2931 <eberefavour250@gmail.com>
2023-03-16 23:54:36 -0700

    4 try

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,11 @@
 #include <stdio.h>
-#include <string.h>
 /**
  * main-A program that print a line;
  * Return: 0 (Success);
  */
 int main(void)
 {
-char s[50];
-strcpy(s, "\"Programming is like building a multilingual puzzle")
-puts(s);
+puts("\"Programming is like building a multilingual puzzle")
 return (0);
 }
 
