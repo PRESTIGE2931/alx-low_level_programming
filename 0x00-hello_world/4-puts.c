53d252071c1a2de499449382c2c816e29c5cb396
PRESTIGE2931 <eberefavour250@gmail.com>
2023-03-17 00:01:38 -0700

    4 agin o

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 /**
- * main-A program that print a line;
+ * main - A program that print a line;
  * Return: 0 (Success);
  */
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle")
+puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }
 
