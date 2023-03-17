3a96687dc2099b6d132bc10c838b865a7f4b3245
PRESTIGE2931 <eberefavour250@gmail.com>
2023-03-16 23:26:47 -0700

    4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
+#include <string.h>
 /**
  * main-A program that print a line;
- * Return: 0;
+ * Return: 0 (Success);
  */
 int main(void)
 {
 puts("\"Progiramming is like building a multilingual puzzle");
 return (0);
 }
-
