46b7bfcacfc22e0138caf0d0eeaff347b78940d0
Ebere Favour <eberefavour250@gmail.com>
2023-06-10 12:09:20 +0100

    second commmit Task1

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
-#include "main.h"
+#include <string.h>
+/* #include "main.h" */
 
 /**
  * main - prints the name of the program
@@ -8,8 +9,11 @@
  *
  * Return: Always 0 (Success)
  */
+
 int main(int argc, char *argv[])
 {
-printf("%s\n", argv[0]);
-return (0);
+    (void)argc;
+    printf("%s",argv[0]);
+    return 0;
 }
+
