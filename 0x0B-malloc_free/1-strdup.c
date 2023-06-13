f5963e8e8b5074c928a71775fbe4d9b777bc8081
PRESTIGE2931 <eberefavour250@gmail.com>
2023-06-13 23:27:19 +0100

    task 1 commit

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include "main.h"
+
+/**
+ * *_strdup(char *str) - returns a pointer to a newly allocated space in memory
+ * @str: string to duplicate
+ *
+ * Returns (pointer_to_duplicate) on success
+ */
+char *_strdup(char *str)
+{
+    char *pointer_to_duplicate;
+    int length = 0;
+    int new;
+    int i = 0;
+
+    if (str == NULL)
+        return (NULL);
+
+    while (isalpha(str[i]) != 0 && str[length] != '\0')
+    {
+        length++;
+    }
+    new = length - 1;
+    
+    pointer_to_duplicate = malloc(sizeof(char) * (new));
+
+    if (pointer_to_duplicate == NULL)
+        return (NULL);
+    else
+        do
+        {
+            pointer_to_duplicate[i] = str[i];
+            i++;
+        } while (str[i] != '\0' && i <= new);
+        
+
+    return (pointer_to_duplicate);
+}
--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -1,5 +1,8 @@
-#if !defined(MACRO)
-#define MACRO
+#ifndef FILE_MAIN
+#define FILE_MAIN
+
 char *create_array(unsigned int size, char c);
 void simple_print_buffer(char *buffer, unsigned int size);
+char *_strdup(char *str);
+
 #endif
