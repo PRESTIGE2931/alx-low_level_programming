60e71d256a7b68a24e6125710eb77636b3fdd7be
PRESTIGE2931 <eberefavour250@gmai.com>
2023-06-13 07:27:18 -0700

    commit

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -4,7 +4,8 @@
 
 /**
 * create_array - creates an array of chars
-* @pointer: the address of memory created
+* @pointer_to_array: the address of memory created
+* @c special charater to initialize the pointer value
 * @size: the size of the memory to print
 *
 * Return: Nothing.
