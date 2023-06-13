50b73eb4dd3249a6a3a7914b5d2db2ba1edcf749
PRESTIGE2931 <eberefavour250@gmai.com>
2023-06-13 07:08:47 -0700

    Checking betty for Task o

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -3,26 +3,25 @@
 #include "main.h"
 
 /**
- * create_array - creates an array of chars
- * @pointer: the address of memory created
- * @size: the size of the memory to print
- *
- * Return: Nothing.
- */
+* create_array - creates an array of chars
+* @pointer: the address of memory created
+* @size: the size of the memory to print
+*
+* Return: Nothing.
+*/
 char *create_array(unsigned int size, char c)
 {
-    unsigned int i = 0;
-    char *pointer_to_array;
-
-    if (size == 0)
-        return NULL;
-    pointer_to_array = malloc(sizeof(char) * size);
-    if (pointer_to_array == NULL)
-        return NULL;
-    while (i < size)
-    {
-        pointer_to_array[i] = c;
-        i++;
-    }
-    return pointer_to_array;
+unsigned int i = 0;
+char *pointer_to_array;
+if (size == 0)
+return (NULL);
+pointer_to_array = malloc(sizeof(char) * size);
+if (pointer_to_array == NULL;
+return (NULL);
+while (i < size)
+{
+pointer_to_array[i] = c;
+i++;
+}
+return (pointer_to_array);
 }
