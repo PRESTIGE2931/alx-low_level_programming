0b83a70678b883491a2bc94266d773b825320e5f
PRESTIGE2931 <eberefavour250@gmail.com>
2023-06-13 12:29:48 +0100

    First commit for task 0

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * create_array - creates an array of chars
+ * @pointer: the address of memory created
+ * @size: the size of the memory to print
+ *
+ * Return: Nothing.
+ */
+char *create_array(unsigned int size, char c)
+{
+    unsigned int i = 0;
+    char *pointer_to_array;
+
+    if (size == 0)
+        return NULL;
+    pointer_to_array = malloc(sizeof(char) * size);
+    if (pointer_to_array == NULL)
+        return NULL;
+    while (i < size)
+    {
+        pointer_to_array[i] = c;
+        i++;
+    }
+    return pointer_to_array;
+}
---
+++ 0x0B-malloc_free/main.h
@@ -0,0 +1,5 @@
+#if !defined(MACRO)
+#define MACRO
+char *create_array(unsigned int size, char c);
+void simple_print_buffer(char *buffer, unsigned int size);
+#endif
