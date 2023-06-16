8b1e954d07675997c4bfb55cc741bc54950d166a
PRESTIGE2931 <eberefavour250@gmail.com>
2023-06-16 08:17:06 +0100

    First test commit for task 0

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/*
+* *malloc_check - allocates memory using malloc
+* @b: this is the number of space to allocate
+*
+* Return (a pointer to the allocated memory) on success and (98) on Error
+*/
+void *malloc_checked(unsigned int b)
+{
+    void *pointer_to_memory;
+
+    pointer_to_memory = malloc(b);
+    if (pointer_to_memory == NULL)
+        exit(98);
+
+    return pointer_to_memory; 
+}
---
+++ 0x0C-more_malloc_free/main.h
@@ -0,0 +1,7 @@
+#if !defined MAIN_FILE
+#define MAIN_FILE
+
+int _putchar(char c);
+void *malloc_checked(unsigned int b);
+
+#endif 
