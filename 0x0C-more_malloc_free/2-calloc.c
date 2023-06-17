43b6da4a14b3442d46569c9da17cbeca5e87ee77
PRESTIGE2931 <eberefavour250@gmai.com>
2023-06-16 20:10:40 -0700

    Fifth commit (betty check)

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -1,6 +1,25 @@
-#include <stdio.h>
 #include <stdlib.h>
 #include "main.h"
+
+/**
+ * *_set - fills memory with a constant byte
+ * @s: memory area to be filled
+ * @hold: char to copy
+ * @n: number of times to copy b
+ *
+ * Return: pointer to the memory area s
+ */
+char *_set(char *s, char hold, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+	{
+		s[i] = hold;
+	}
+
+	return (s);
+}
 
 /**
  * *_calloc - allocates memory for an array
@@ -11,30 +30,17 @@
  */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-char *pointer_to_array;
-if (nmemb == 0 || size == 0)
-return (NULL);
-pointer_to_array = malloc(size * nmemb);
-if (pointer_to_array == NULL)
-return (NULL);
-_set(pointer_to_array, 0, nmemb * size);
-return (pointer_to_array);
-}
+	char *ptr;
 
-/**
- * *_set - fills memory with a constant byte
- * @s: memory area to be filled
- * @b: char to copy
- * @n: number of times to copy b
- *
- * Return: pointer to the memory area s
- */
-char *_set(char *array, char val, unsigned int n)
-{
-unsigned int i;
-for (i = 0; i < n; i++)
-{
-array[i] = val;
-}
-return (array);
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+
+	ptr = malloc(size * nmemb);
+
+	if (ptr == NULL)
+		return (NULL);
+
+	_set(ptr, 0, nmemb * size);
+
+	return (ptr);
 }
