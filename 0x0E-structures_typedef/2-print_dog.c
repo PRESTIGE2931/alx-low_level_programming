84118b0e048c18b9a517cfb5d925e378fbbc2d6a
PRESTIGE2931 <eberefavour250@gmail.com>
2023-06-20 02:10:54 +0100

    Second commit for task 1 and 2

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -6,15 +6,18 @@
  * print_dog - prints a struct dog
  * @d: struct dog to print
  */
-void print_dog(struct dog *d)
-{
-if (d == NULL)
-return (NULL);
+struct dog {
+    char *name;
+    float age;
+    char *owner;
+};
 
-if (d->name == NULL)
-d->name = "(nil)";
-if (d->owner == NULL)
-d->owner = "(nil)";
+void print_dog(struct dog *d) {
+    if (d == NULL) {
+        return;
+    }
 
-printf("Name: %s\nAge: %f\nOwner: %s\n", d->name, d->age, d->owner);
+    printf("Name: %s\n", d->name != NULL ? d->name : "(nil)");
+    printf("Age: %.1f\n", d->age);
+    printf("Owner: %s\n", d->owner != NULL ? d->owner : "(nil)");
 }
