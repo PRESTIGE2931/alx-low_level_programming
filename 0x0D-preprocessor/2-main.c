a112f0e91cf3094fcda03991f380c33351441cf3
PRESTIGE2931 <eberefavour250@gmail.com>
2023-06-20 02:23:48 +0100

    commit

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,9 +1,12 @@
 #include <stdio.h>
 
-/* main- prints out the current file location */
-
+/**
+ * main - prints the name of the file
+ *
+ * Return: (Always 0 Success)
+ */
 int main(void)
 {
-printf("%s\n", __FILE__);
-return (0);
+	printf("%s\n", __FILE__);
+	return (0);
 }
