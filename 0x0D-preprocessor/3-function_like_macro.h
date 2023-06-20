ca6afb5a4567f87dcd115cc937c70fad946d8417
PRESTIGE2931 <eberefavour250@gmail.com>
2023-06-20 02:46:16 +0100

    commit

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,6 +1,13 @@
 #ifndef HEADER_FILE
 #define HEADER_FILE
 
-#define ABS(X) ((X) < 0 ? (X  * -1) : (X))
+#define ABS(X) ((X) < (0) ? (X  * -1) : (X))
+
+#endif
+
+#ifndef FUNCTION_LIKE_MACRO_H
+#define FUNCTION_LIKE_MACRO_H
+
+#define ABS(x) ((x) < (0) ? -(x) : (x))
 
 #endif
