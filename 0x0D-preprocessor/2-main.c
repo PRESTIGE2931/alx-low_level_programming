d89d594b2f1b669c805daf098343571f0b19d13d
PRESTIGE2931 <eberefavour250@gmail.com>
2023-06-20 02:16:35 +0100

    commit for task 2

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
 
-/* main- prints out the current file location */
+/**
+ * main -  prints out the current file location 
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 printf("%s\n", __FILE__);
