c6c12c3106f6f5aa9d4671377d13ff35a78af460
PRESTIGE2931 <eberefavour250@gmail.com>
2023-06-22 00:28:37 +0100

    Fifth commit Task 0

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,9 +1,13 @@
 #include "function_pointers.h"
 
+/**
+ * print_name - prints a name
+ * @name: name to print
+ * @f: pointer to the printing function
+ */
 void print_name(char *name, void (*f)(char *))
 {
-if (!name || !f)
-return;
-f(name);
-return;
+		if (!name || !f)
+			return;
+		f(name);
 }
