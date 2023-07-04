6f3dc9365c6579b83dcf7f374a9821c0f97650c6
PRESTIGE2931 <eberefavour250@gmail.com>
2023-07-04 03:12:43 +0100

    Task 0

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,25 +1,5 @@
 #include "lists.h"
 
-
-size_t print_list(const list_t *h)
-{
-int i;
-size_t count = 0;
-
-const list_t *element = h;
-for (i = 0; element; i++)
-{
-if (element->str == NULL)
-printf("[0] (nil)\n");
-else
-printf("[%d] %s\n", element->len, element->str);
-element = element->next;
-count++;
-}
-return (count);
-}
-
-
 /**
  * print_list - prints all the 
  * elements of a list_t list function
@@ -28,18 +8,18 @@ return (count);
 */
 size_t print_list(const list_t *h)
 {
-	size_t len = 0;
-	int count;
-	const list_t *c = h;
+size_t len = 0;
+int count;
+const list_t *c = h;
 
-	for (count = 0; c; count++)
-	{
-		if (c->str == NULL)
-			printf("[0] (nil)\n");
-		else
-			printf("[%d] %s\n", c->len, c->str);
-	c = c->next;
-	len++;
-	}
+for (count = 0; c; count++)
+{
+if (c->str == NULL)
+printf("[0] (nil)\n");
+else
+printf("[%d] %s\n", c->len, c->str);
+c = c->next;
+len++;
+}
 return (len);
 }
