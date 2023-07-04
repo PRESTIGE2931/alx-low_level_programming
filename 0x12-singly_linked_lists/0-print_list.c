91d858615117f31363dba28976e05e6c7e045afd
PRESTIGE2931 <eberefavour250@gmail.com>
2023-07-03 19:06:58 -0700

    Third betty check

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,7 +1,8 @@
 #include "lists.h"
 
 /**
- * print_list - prints all the elements of a list_t list function
+ * print_list - prints all the 
+ * elements of a list_t list function
  * @h: pointer
  * Return: (count) number of nodes in the list
 */
@@ -9,8 +10,8 @@ size_t print_list(const list_t *h)
 {
 int i;
 size_t count = 0;
-const list_t *element = h;
 
+const list_t *element = h;
 for (i = 0; element; i++)
 {
 if (element->str == NULL)
