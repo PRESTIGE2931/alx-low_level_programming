d43bd27b476bdec93825a054d22a283483964656
PRESTIGE2931 <eberefavour250@gmail.com>
2023-07-03 18:43:31 -0700

    Advance task 100

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+void contructor_toFunction(void)__attribute__((constructor));
+
+/**
+ * hare - Prints a string before the
+ *        main function is executed.
+ */
+void contructor_toFunction(void)
+{
+	printf("You're beat! and yet, you must allow,\n"
+	       "I bore my house upon my back!\n");
+}
