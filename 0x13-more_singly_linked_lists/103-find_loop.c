af952285557d427d751872fbadc9fd7c7a70b7e5
PRESTIGE2931 <eberefavour250@gmail.com>
2023-07-07 05:42:08 +0100

    TASK 14

--- 0x13-more_singly_linked_lists/103-find_loop.c
+++ 0x13-more_singly_linked_lists/103-find_loop.c
@@ -9,28 +9,28 @@
  */
 listint_t *find_listint_loop(listint_t *head)
 {
-	listint_t *present, *forward;
+listint_t *present, *forward;
 
-	if (head == NULL)
-		return (NULL);
-	if(head->next == NULL)
-		return (NULL);
-	forward = (head->next)->next;
-	present = head->next;
-	while (forward != NULL)
-	{
-		if (present == forward)
-		{
-			present = head;
-			while (present != forward)
-			{
-				present = present->next;
-				forward = forward->next;
-			}
-			return (present);
-		}
-		present = present->next;
-		forward = (forward->next)->next;
-	}
-	return (NULL);
+if (head == NULL)
+return (NULL);
+if(head->next == NULL)
+return (NULL);
+forward = (head->next)->next;
+present = head->next;
+while (forward != NULL)
+{
+if (present == forward)
+{
+present = head;
+while (present != forward)
+{
+present = present->next;
+forward = forward->next;
+}
+return (present);
+}
+present = present->next;
+forward = (forward->next)->next;
+}
+return (NULL);
 }
