02570b189f43b0bb3acf81d27d09732acb8e90df
PRESTIGE2931 <eberefavour250@gmail.com>
2023-07-08 18:22:25 +0100

    Sixth commit

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -7,21 +7,20 @@
 unsigned int binary_to_uint(const char *b)
 {
 int i;
-unsigned int num;
+unsigned int d = 0;
 
-num = 0;
 if (!b)
 return (0);
-for (i = 0; b[i] != '\0'; i++)
+for (i = 0 ; b[i] != '\0' ; i++)
 {
-if (b[i] != '0' && b[i] != '1')
+if (b[i] != '1' && b[i] != '0')
 return (0);
 }
-for (i = 0; b[i] != '\0'; i++)
+for (i = 0 ; b[i] != '\0' ; i++)
 {
-num <<= 1;
+d <<= 1;
 if (b[i] == '1')
-num += 1;
+d += 1;
 }
-return (num);
+return (d);
 }
