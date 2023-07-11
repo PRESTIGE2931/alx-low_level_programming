b2e63178b304715d480f737a279290be51479f70
PRESTIGE2931 <eberefavour250@gmail.com>
2023-07-11 02:05:29 +0100

    Third commit for task 0

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -11,26 +11,23 @@
 */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
+ssize_t O, R, W;
 char *buffer;
-ssize_t openFile, readFile, writeFile;
-signed int count;
 
-if(filename == NULL)
+if (filename == NULL)
 return (0);
-openFile = open(filename, O_RDWR);
-if(openFile == -1)
+buffer = malloc(sizeof(char) * letters);
+if (buffer == NULL)
 return (0);
-buffer = malloc(sizeof((char) *buffer));
-if(buffer == NULL)
-return (0);
-count = sizeof((char) *buffer);
-readFile = read(openFile, buffer, letters);
-writeFile = write(STDOUT_FILENO, buffer, readFile);
-if(writeFile < count){
+O = open(filename, O_RDONLY);
+R = read(O, buffer, letters);
+W = write(STDOUT_FILENO, buffer, R);
+if (O == -1 || R == -1 || W == -1 || W != R)
+{
 free(buffer);
 return (0);
 }
 free(buffer);
-close(openFile);
-return (writeFile);
+close(O);
+return (W);
 }
